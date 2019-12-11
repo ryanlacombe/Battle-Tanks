73e3b7824712041ee9dde090a114e2d93dbd3bdf
Ryan LaCombe <ryanalacombe@gmail.com>
2019-12-11 15:16:46 -0600

    Added a reload condition to firing shells

--- GraphicalTestApp/Cannon.cs
+++ GraphicalTestApp/Cannon.cs
@@ -10,6 +10,8 @@ namespace GraphicalTestApp
     {
         private Sprite _cannonSprite;
         private int _playerNum;
+        private int _shellCount1;
+        private int _shellCount2;
 
         public Cannon(float x, float y, int playerNum)
         {
@@ -45,28 +47,44 @@ namespace GraphicalTestApp
         {
             if (Input.IsKeyPressed(70) && _playerNum == 0)
             {
-                Shell shell = new Shell(XAbsolute, YAbsolute, _playerNum);
-                Vector3 direction = GetDirectionAbsolute();
+                if(_shellCount1 < 5)
+                {
+                    Shell shell = new Shell(XAbsolute, YAbsolute, _playerNum);
+                    Vector3 direction = GetDirectionAbsolute();
 
-                shell.Rotate(GetRotationAbsolute());
-                shell.X += direction.x * -50f;
-                shell.Y += direction.y * -50f;
-                shell.XVelocity += direction.x * -300f;
-                shell.YVelocity += direction.y * -300f;
-                Parent.Parent.AddChild(shell);
+                    shell.Rotate(GetRotationAbsolute());
+                    shell.X += direction.x * -50f;
+                    shell.Y += direction.y * -50f;
+                    shell.XVelocity += direction.x * -300f;
+                    shell.YVelocity += direction.y * -300f;
+                    Parent.Parent.AddChild(shell);
+                    _shellCount1++;
+                }
+            }
+            else if(Input.IsKeyPressed(82) && _playerNum == 0)
+            {
+                _shellCount1 = 0;
             }
 
             else if (Input.IsKeyPressed(59) && _playerNum == 1)
             {
-                Shell shell = new Shell(XAbsolute, YAbsolute, _playerNum);
-                Vector3 direction = GetDirectionAbsolute();
+                if (_shellCount2 < 5)
+                {
+                    Shell shell = new Shell(XAbsolute, YAbsolute, _playerNum);
+                    Vector3 direction = GetDirectionAbsolute();
 
-                shell.Rotate(GetRotationAbsolute());
-                shell.X += direction.x * -50f;
-                shell.Y += direction.y * -50f;
-                shell.XVelocity += direction.x * -300f;
-                shell.YVelocity += direction.y * -300f;
-                Parent.Parent.AddChild(shell);
+                    shell.Rotate(GetRotationAbsolute());
+                    shell.X += direction.x * -50f;
+                    shell.Y += direction.y * -50f;
+                    shell.XVelocity += direction.x * -300f;
+                    shell.YVelocity += direction.y * -300f;
+                    Parent.Parent.AddChild(shell);
+                    _shellCount2++;
+                }
+            }
+            else if (Input.IsKeyPressed(80) && _playerNum == 1)
+            {
+                _shellCount2 = 0;
             }
         }
 
---
+++ ReadMe.txt
@@ -0,0 +1,35 @@
+Tank Game ReadMe:
+
+Launching the Game:
+
+Open the folder and double click "GraphicalTestApp.exe."
+|-------------------------------------------------------------------------------------------------------------------------|
+
+Playing the game:
+
+There are two tanks that two players can play. 
+	Player 1 plays the lower left tank and uses the W,A,S,D,Q,E,F, and R keys.
+		Use the W key to move forwards in the direction the tank is facing.
+		Use the A key to turn the tank counter-clockwise.
+		Use the S key to move in reverse from the direction the tank is facing.
+		Use the D key to turn the tank clockwise.
+		Use the Q key to turn the cannon counter-clockwise.
+		Use the E key to turn the cannon clockwise.
+		Use the F key to fire a shell in the direction the cannon is facing.
+			(You can only shoot 5 shells before having to reload.)
+		Use the R to reload your shells.
+
+	Player 2 plays the upper right tank and uses the I,J,K,L,U,O,;, and P keys.
+		Use the I key to move forwards in the direction the tank is facing.
+		Use the J key to turn the tank counter-clockwise.
+		Use the K key to move in reverse from the direction the tank is facing.
+		Use the L key to turn the tank clockwise.
+		Use the U key to turn the cannon counter-clockwise.
+		Use the O key to turn the cannon clockwise.
+		Use the ; key to fire a shell in the direction the cannon is facing.
+			(You can only shoot 5 shells before having to reload.)
+		Use the P to reload your shells.
+
+Press the "Esc" key to close the game.
+
+|-------------------------------------------------------------------------------------------------------------------------|
